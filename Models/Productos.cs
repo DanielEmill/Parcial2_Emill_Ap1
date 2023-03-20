a453471fa97c8b55b6b80848b2c3c600ad27633e
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-03-20 18:11:50 -0400

    Models Agregado + Carpeta Creadas

---
+++ Models/Productos.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Productos{
+    [Key]
+    public int ProductoId { get; set; }
+    [Required(ErrorMessage = "La descripcion es obligatorio.")]
+    public string? Descripcion { get; set; }
+    [Range(1, Double.MaxValue, ErrorMessage ="El Costo debe de estar en el rango de {1} - {2}")]
+    public double Costo { get; set; }
+    [Range(1, Double.MaxValue, ErrorMessage ="El Precio debe de estar en el rango de {1} - {2}")]
+    public double Precio { get; set; }
+    [Range(1, Double.MaxValue, ErrorMessage ="La Existencia debe de estar en el rango de {1} - {2}")]
+    public int Existencia { get; set; }
+}
