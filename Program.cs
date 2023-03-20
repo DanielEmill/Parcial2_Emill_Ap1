4f564963894dec04c792120c6f68f820fd1f6288
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-03-20 17:59:09 -0400

    Limpiando el proyecto

--- Program.cs
+++ Program.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using Parcial2_Emill.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
